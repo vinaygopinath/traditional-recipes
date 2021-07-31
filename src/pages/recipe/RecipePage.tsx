26c9ae9a9aeca9d725f3fe454e92cdab6713d581
Vinay Gopinath <vinay@vinaygopinath.me>
2021-07-31 23:32:37 +0300

    Refactor banner content into component

--- src/pages/recipe/RecipePage.tsx
+++ src/pages/recipe/RecipePage.tsx
@@ -1,9 +1,8 @@
-import { Box, Grid, Image, ResponsiveContext } from "grommet"
+import { Box, ResponsiveContext } from "grommet"
 import React from "react"
 import { withTranslation, WithTranslation } from "react-i18next"
 import { connect, ConnectedProps } from "react-redux"
 import { RouteComponentProps } from "react-router-dom"
-import Separator from "../../common-components/separator/Separator"
 import { CountryProp } from "../../models/CountryProp"
 import { Recipe } from "../../models/Recipe"
 import { RecipeActionCreators } from "../../store/recipe/RecipeActionCreators"
@@ -11,7 +10,7 @@ import { RootState } from "../../store/RootReducer"
 import { CountryUtils } from "../../utils/CountryUtils"
 import { RecipeUtils } from "../../utils/RecipeUtils"
 import { TextUtils } from "../../utils/TextUtils"
-import "./RecipePage.scss"
+import RecipeBanner from "./recipe-banner/RecipeBanner"
 
 const mapState = (state: RootState) => ({
   recipe: state.recipe.recipe,
@@ -76,71 +75,6 @@ class RecipePage extends React.PureComponent<RecipePageProps, RecipeState> {
     )
   }
 
-  getSubtitle(recipe: Recipe): string {
-    if (recipe.subtitle) {
-      return this.getRecipeString(recipe.subtitle)
-    } else {
-      return ""
-    }
-  }
-
-  getBannerColumnsForSize(size: string): string[] {
-    switch (size) {
-      case "xsmall":
-      case "small":
-        // Phone screens have a single full-width column
-        return ["full"]
-      default:
-        // Tablet and desktop screens have two columns
-        // image in the first, and intro in the second
-        return ["1/2", "1/2"]
-    }
-  }
-
-  getBannerRowsForSize(size: string): string[] {
-    switch (size) {
-      case "xsmall":
-      case "small":
-        return ["auto", "auto"]
-      default:
-        return ["auto"]
-    }
-  }
-
-  getBannerAreasForSize(
-    size: string
-    ): { name?: string; start?: number[]; end?: number[] }[] {
-      switch (size) {
-        case "xsmall":
-        case "small":
-          return [
-            /**
-             * Small screens have two full-width rows
-             */
-            { name: "image", start: [0, 0], end: [0, 0] },
-            { name: "intro", start: [0, 1], end: [0, 1] }
-          ]
-        default:
-          /**
-           * Medium, large and xlarge screens use a single row + two columns
-           */
-          return [
-            { name: "intro", start: [0, 0], end: [0, 0] },
-            { name: "image", start: [1, 0], end: [1, 0] }
-          ]
-      }
-    }
-
-    getBannerAreaGapBySize(size: string) {
-      switch (size) {
-        case "xsmall":
-        case "small":
-          return "none"
-        default:
-          return "large"
-      }
-    }
-
   render() {
     const recipe = this.props.recipe
     if (!recipe || !this.state.isTranslationReady) {
@@ -151,28 +85,7 @@ class RecipePage extends React.PureComponent<RecipePageProps, RecipeState> {
       <ResponsiveContext.Consumer>
         {(size) => (
           <Box>
-            <Box className="banner" background="light-2">
-              <Grid
-                columns={this.getBannerColumnsForSize(size)}
-                rows={this.getBannerRowsForSize(size)}
-                areas={this.getBannerAreasForSize(size)}
-                gap={this.getBannerAreaGapBySize(size)}>
-                <Box gridArea="intro" className="intro">
-                  <h1 className="title">{this.getRecipeString(recipe.title)}</h1>
-                  <span className="subtitle">{this.getSubtitle(recipe)}</span>
-                  <Separator pad="medium"/>
-                  <p className="description max-width-90">
-                    {this.getRecipeString(recipe.description)}
-                  </p>
-                </Box>
-                <Image
-                  gridArea="image"
-                  className="image"
-                  src={recipe.images[0].url}
-                  fit="cover"
-                />
-              </Grid>
-            </Box>
+            <RecipeBanner size={size} recipe={recipe} getRecipeString={translationKey => this.getRecipeString(translationKey)} />
             <div>Recipe title: {this.getRecipeString(recipe.title)}</div>
             <div>
               Recipe ingredients:{" "}
---
+++ src/pages/recipe/recipe-banner/RecipeBanner.tsx
@@ -0,0 +1,112 @@
+import { Box, Grid, Image } from "grommet"
+import React from "react"
+import { Recipe } from "../../../models/Recipe"
+import Separator from "../../../common-components/separator/Separator"
+
+type RecipeBannerProps = {
+  recipe: Recipe
+  size: string
+  getRecipeString: (translationKey: string) => string
+}
+
+export default class RecipeBanner extends React.PureComponent<RecipeBannerProps> {
+  getBannerColumnsForSize(): string[] {
+    switch (this.props.size) {
+      case "xsmall":
+      case "small":
+        // Phone screens have a single full-width column
+        return ["full"]
+      default:
+        // Tablet and desktop screens have two columns
+        // image in the first, and intro in the second
+        return ["1/2", "1/2"]
+    }
+  }
+
+  getBannerRowsForSize(): string[] {
+    switch (this.props.size) {
+      case "xsmall":
+      case "small":
+        return ["auto", "auto"]
+      default:
+        return ["auto"]
+    }
+  }
+
+  getBannerAreasForSize(): {
+    name?: string
+    start?: number[]
+    end?: number[]
+  }[] {
+    switch (this.props.size) {
+      case "xsmall":
+      case "small":
+        return [
+          /**
+           * Small screens have two full-width rows
+           */
+          { name: "image", start: [0, 0], end: [0, 0] },
+          { name: "intro", start: [0, 1], end: [0, 1] },
+        ]
+      default:
+        /**
+         * Medium, large and xlarge screens use a single row + two columns
+         */
+        return [
+          { name: "intro", start: [0, 0], end: [0, 0] },
+          { name: "image", start: [1, 0], end: [1, 0] },
+        ]
+    }
+  }
+
+  getBannerAreaGapBySize() {
+    switch (this.props.size) {
+      case "xsmall":
+      case "small":
+        return "none"
+      default:
+        return "large"
+    }
+  }
+
+  getRecipeString(translationKey: string) {
+    return this.props.getRecipeString(translationKey)
+  }
+
+  getSubtitle(recipe: Recipe): string {
+    if (recipe.subtitle) {
+      return this.getRecipeString(recipe.subtitle)
+    } else {
+      return ""
+    }
+  }
+
+  render() {
+    const recipe = this.props.recipe
+    return (
+      <Box className="banner" background="light-2">
+        <Grid
+          columns={this.getBannerColumnsForSize()}
+          rows={this.getBannerRowsForSize()}
+          areas={this.getBannerAreasForSize()}
+          gap={this.getBannerAreaGapBySize()}
+        >
+          <Box gridArea="intro" className="intro">
+            <h1 className="title">{this.getRecipeString(recipe.title)}</h1>
+            <span className="subtitle">{this.getSubtitle(recipe)}</span>
+            <Separator pad="medium" />
+            <p className="description max-width-90">
+              {this.getRecipeString(recipe.description)}
+            </p>
+          </Box>
+          <Image
+            gridArea="image"
+            className="image"
+            src={recipe.images[0].url}
+            fit="cover"
+          />
+        </Grid>
+      </Box>
+    )
+  }
+}
